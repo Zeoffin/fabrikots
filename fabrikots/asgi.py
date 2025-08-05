1248ae6f5b6d881f47c8a4b783e6555f866502d1
Zeo <markuss299@gmail.com>
2025-08-05 13:53:04 +0300

    asgi error

--- fabrikots/asgi.py
+++ fabrikots/asgi.py
@@ -12,12 +12,18 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from fbkquizz.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fabrikots.settings')
 
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
+# Import websocket patterns after Django is set up
+from fbkquizz.routing import websocket_urlpatterns
+
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': django_asgi_app,
     'websocket': AuthMiddlewareStack(
         URLRouter(websocket_urlpatterns)
     )
